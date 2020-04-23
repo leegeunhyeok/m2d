50d71c6f12bae6c2046baec429ce61d9f501593b
leegeunhyeok <dev.ghlee@gmail.com>
2020-04-23 23:54:06 +0900

    UPDATE: Base code

--- main.js
+++ main.js
@@ -2,11 +2,20 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+require('electron-reload')(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+})
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    title: 'M2D',
+    width: 150,
+    height: 150,
+    resizable: false,
+    minimizable: false,
+    maximizable: false,
+    titleBarStyle: 'hidden',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -16,13 +25,14 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
+app.allowRendererProcessReuse = false
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
